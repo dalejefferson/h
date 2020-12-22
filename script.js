589e8cd9ad1f5e1e8dd12d29f4bba143a6e5a644
dalejefferson <66589962+dalejefferson@users.noreply.github.com>
2020-12-22 07:33:46 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+anime({
+    targets: '.yo',
+    translateX: [
+    {value: 200,durationL: 500},
+    {value: 0,duration: 500}
+    ],
+    rotate: '1turn',
+    left: '240px',
+    backgroundColor: '#FFF',
+    borderRadius: ['0%', '25%'],
+    easing: 'easeInOutQuad',
+    points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+    baseFrequency: 0,
+    scale: 1,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutExpo'
+});
